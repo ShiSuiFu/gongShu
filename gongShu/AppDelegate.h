75738130f51ad83ef8076eb533ee76a1f8f3996b
付朋华 <fupenghua@172-10-130-101.lightspeed.sndgca.sbcglobal.net>
2015-11-18 10:05:23 +0800

    Initial Commit

---
+++ gongShu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gongShu
+//
+//  Created by 付朋华 on 15/11/18.
+//  Copyright © 2015年 付朋华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gongShu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gongShu
+//
+//  Created by 付朋华 on 15/11/18.
+//  Copyright © 2015年 付朋华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
